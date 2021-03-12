9e407d017883a1d0ff4f58abd22900a4565b49aa
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-12 00:53:23 +0000

    pages have been added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser= require('body-parser')
 const app=express();
 
 var items=['Buy Food','Eat Food','Cook Food']
-
+let workIems=[]
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -21,17 +21,36 @@ app.get('/',(req,res)=>{
     }
     var day=today.toLocaleDateString('en-US',options)
 
-    res.render('list',{kindOfDay:day,newListItems:items});
+    res.render('list',{listTitle:day,newListItems:items});
 });
 
 app.post('/',function(req,res){
+    
+    let item=req.body.newItem;
 
-     item=req.body.newItem
-     items.push(item)
-    res.redirect('/')
+    if(req.body.list==='Work'){
+        workIems.push(item)
+        res.redirect('/work');
+    }else{  
+        items.push(item);
+        res.redirect('/');
+    }
+    
+});
+
+app.get('/work',function(req,res){
+    res.render('list',{listTitle:'Work List',newListItems:workIems});
+});
+
+app.post('/work',function(res,req){
+    let item=req.body.newItem;
+    workIems.push(item);
+    res.redirect('/work');
 })
 
-
+app.get('/about',function(req,res){
+    res.render('about')
+})
 
 app.listen(3000,function(){
     console.log('Server on port 3000')
